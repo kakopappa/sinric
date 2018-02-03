9bb2aa789a6e84cadcfd92aa430ba84f7ab69b7b
Aruna <aruna.tennakoon@gmail.com>
2018-02-03 13:10:48 +0700

    0.2 more details added

--- arduino_examples/switch_example.ino
+++ arduino_examples/switch_example.ino
@@ -1,52 +1,106 @@
-#include <Arduino.h>
+/*
+ Version 0.2 - Feb 03 2018
+*/ 
 
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-#include <WebSocketsClient.h> // get it from https://github.com/Links2004/arduinoWebSockets
-#include <Hash.h>
-#include <ArduinoJson.h> // get it from https://arduinojson.org/
+#include <WebSocketsClient.h> //  get it from https://github.com/Links2004/arduinoWebSockets/releases 
+#include <ArduinoJson.h> // get it from https://arduinojson.org/ or install via Arduino library manager
 
 ESP8266WiFiMulti WiFiMulti;
 WebSocketsClient webSocket;
 
-void turnOn() {
-  Serial.println("Turn on ...");
+#define MyApiKey "xxxxxxxxxxxxxxxx" // TODO: Change to your sinric API Key. Your API Key is displayed on sinric.com dashboard
+#define MySSID "xxxxxxxxxxxxxxxx" // TODO: Change to your Wifi network SSID
+#define MyWifiPassword "xxxxxxxxxxxxxxxx" // TODO: Change to your Wifi network password
+#define HEARTBEAT_INTERVAL 35000
+
+uint64_t heartbeatTimestamp = 0;
+bool isConnected = false;
+
+void turnOn(String deviceId) {
+  if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of first device
+  {  
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+  } 
+  else if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of second device
+  { 
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+  }
+  else {
+    Serial.print("Turn on for unknown device id: ");
+    Serial.println(deviceId);    
+  }     
 }
 
-void turnOff() {
-  Serial.println("Turn off ...");
+void turnOff(String deviceId) {
+   if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of first device
+   {  
+     Serial.print("Turn off Device ID: ");
+     Serial.println(deviceId);
+   }
+   else if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of second device
+   { 
+     Serial.print("Turn off Device ID: ");
+     Serial.println(deviceId);
+  }
+  else {
+     Serial.print("Turn off for unknown device id: ");
+     Serial.println(deviceId);    
+  }
 }
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
-
   switch(type) {
     case WStype_DISCONNECTED:
-      Serial.printf("[WSc] Disconnected!\n");
+      isConnected = false;    
+      Serial.printf("[WSc] Webservice disconnected from sinric.com!\n");
       break;
     case WStype_CONNECTED: {
-        Serial.printf("[WSc] Connected to url: %s\n", payload);
+      isConnected = true;
+      Serial.printf("[WSc] Service connected to sinric.com at url: %s\n", payload);
+      Serial.printf("Waiting for commands from sinric.com ...\n");        
       }
       break;
     case WStype_TEXT: {
         Serial.printf("[WSc] get text: %s\n", payload);
-  
-        // Payload for ON :
-        //{"deviceId":"5a2b908a74a6703928abf38b","powerState":"ON"}
+        // Example payloads
+
+        // For Switch or Light device types
+        // {"deviceId": xxxx, "action": "setPowerState", value: "ON"} // https://developer.amazon.com/docs/device-apis/alexa-powercontroller.html
+
+        // For Light device type
+        // {"deviceId": xxxx, "action": "AdjustBrightness", value: 3} // https://developer.amazon.com/docs/device-apis/alexa-brightnesscontroller.html
+        // {"deviceId": xxxx, "action": "setBrightness", value: 42} // https://developer.amazon.com/docs/device-apis/alexa-brightnesscontroller.html
+        // {"deviceId": xxxx, "action": "SetColor", value: {"hue": 350.5,  "saturation": 0.7138, "brightness": 0.6501}} // https://developer.amazon.com/docs/device-apis/alexa-colorcontroller.html
+        // {"deviceId": xxxx, "action": "DecreaseColorTemperature", value: 0} // https://developer.amazon.com/docs/device-apis/alexa-colortemperaturecontroller.html
+        // {"deviceId": xxxx, "action": "IncreaseColorTemperature", value: 0} // https://developer.amazon.com/docs/device-apis/alexa-colortemperaturecontroller.html
+        // {"deviceId": xxxx, "action": "SetColorTemperature", value: 2200} // https://developer.amazon.com/docs/device-apis/alexa-colortemperaturecontroller.html
+        
         DynamicJsonBuffer jsonBuffer;
-        JsonObject& json = jsonBuffer.parseObject((char*)payload);
-  
-        String deviceId = json ["deviceId"]; // NOTE: You can identify multiple switches by deviceId. 
-        // Every Smart Home Device you create in sinric.com has a unique device id.       
-        String action = json ["action"]; 
-
-        if(action == "setPowerState") {
+        JsonObject& json = jsonBuffer.parseObject((char*)payload); 
+        String deviceId = json ["deviceId"];     
+        String action = json ["action"];
+        
+        if(action == "setPowerState") { // Switch or Light
             String value = json ["value"];
-
             if(value == "ON") {
-                turnOn();
+                turnOn(deviceId);
             } else {
-                turnOff();
+                turnOff(deviceId);
             }
+        }
+        else if(action == "setBrightness") {
+          
+        }
+        else if(action == "AdjustBrightness") {
+          
+        }
+        else if (action == "test") {
+            Serial.println("[WSc] received test command from sinric.com");
         }
       }
       break;
@@ -54,18 +108,26 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
       Serial.printf("[WSc] get binary length: %u\n", length);
       break;
   }
-
 }
 
 void setup() {
   Serial.begin(115200);
   
-  // TODO: Change your WiFI settings
-  WiFiMulti.addAP("<your wifi ssid>", "<your wifi password>");
+  WiFiMulti.addAP(MySSID, MyWifiPassword);
+  Serial.println();
+  Serial.print("Connecting to Wifi: ");
+  Serial.println(MySSID);  
 
-  //WiFi.disconnect();
+  // Waiting for Wifi connect
   while(WiFiMulti.run() != WL_CONNECTED) {
-    delay(100);
+    delay(500);
+    Serial.print(".");
+  }
+  if(WiFiMulti.run() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.print("WiFi connected. ");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
   }
 
   // server address, port and URL
@@ -73,13 +135,22 @@ void setup() {
 
   // event handler
   webSocket.onEvent(webSocketEvent);
-
-  // TODO: Change your API Key. Your API Key is displayed on sinric.com dashboard
-  webSocket.setAuthorization("apikey", "<Place your API key here>");
+  webSocket.setAuthorization("apikey", MyApiKey);
   
- 
+  // try again every 5000ms if connection has failed
+  webSocket.setReconnectInterval(5000);   // If you see 'class WebSocketsClient' has no member named 'setReconnectInterval' error update arduinoWebSockets
 }
 
 void loop() {
   webSocket.loop();
+  
+  if(isConnected) {
+      uint64_t now = millis();
+      
+      // Send heartbeat in order to avoid disconnections during ISP resetting IPs over night. Thanks @MacSass
+      if((now - heartbeatTimestamp) > HEARTBEAT_INTERVAL) {
+          heartbeatTimestamp = now;
+          webSocket.sendTXT("H");          
+      }
+  }   
 }
