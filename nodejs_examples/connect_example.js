f17447a8f402df4f7e04dbed06395ab6ed733705
Aruna <aruna.tennakoon@gmail.com>
2018-03-04 18:19:57 +0700

    updated with more details

--- nodejs_examples/connect_example.js
+++ nodejs_examples/connect_example.js
@@ -1,5 +1,5 @@
 const WebSocket = require('ws');
-const token = "your api key here";
+const token = "Your api key here";
 
 const options = {
     headers: {
@@ -11,4 +11,18 @@ const ws = new WebSocket('ws://iot.sinric.com', options);
  
 ws.on('open', function open() {
    console.log("Connected. waiting for commands..");
+});
+
+ws.on('message', function incoming(data) {
+   console.log("Request : " + data)
+   let cmdObj = JSON.parse(data);
+
+   if(cmdObj.action == "setPowerState") {
+       if(cmdObj.value === "ON") {
+           console.log("Turn on...")
+       } else {
+           console.log("Turn off...")
+       } 
+   }
+
 });
